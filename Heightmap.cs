29f40e90b9ae6dc860e8fdc775a3c85ea313f216
Andrej Bestuzhev <andrej.bestuzhev@gmail.com>
2017-04-01 11:40:37 +0300

    Init

---
+++ Heightmap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Terrain
+{
+    public class Heightmap
+    {
+        protected Bitmap HeightmapTexture;
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+
+        protected Random Random;
+
+        public Heightmap(int width, int height, int seed)
+        {
+            this.HeightmapTexture = new Bitmap(width, height);
+            this.Width = width;
+            this.Height = height;
+            this.Random = new Random(seed);
+        }
+    }
+}
---
+++ TerrainType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terrain
+{
+    public enum TerrainType
+    {
+        Shore,
+        Plains,
+        Rivers,
+        Mountains,
+        Hills, 
+        Island
+    }
+}
